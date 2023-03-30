ee23f4d391077c9a22bf58cf345e80ff6c1a22f2
COMP3100 Maximus Moessis <maximus.moessis@students.mq.edu.au>
2023-03-30 06:50:18 +0000

    Reverted back to original, didn't want to lose the cleaner version that used functions but can't use it.

--- LRR.java
+++ LRR.java
@@ -1,95 +1,88 @@
-import java.io.*;
+import java.io.*;  
 import java.net.*; 
- 
+
 public class LRR {  
 	public static void main(String[] args) {  
 		try {      
 			Socket s = new Socket("localhost", 50000);  
 			DataOutputStream dout = new DataOutputStream(s.getOutputStream());
 			BufferedReader dis = new BufferedReader(new InputStreamReader(s.getInputStream()));  
-			
+
 			String str; // String used to read the BufferedReader Stream.
 
-			sendMsg("HELO", dout); 
-		
-			str = recMsg(dis);
-			
+			System.out.println("Client: HELO\n");
+			dout.write(("HELO\n").getBytes());  
+			dout.flush();  
+
+			str = (String)dis.readLine();
+			System.out.println("Server: " + str);
+
 			String username = System.getProperty("user.name"); 
-	
-			sendMsg("AUTH "+ username, dout);
 
-			str = recMsg(dis);
+			System.out.println("Client: AUTH pooch\n");
+			dout.write(("AUTH "+ username +"\n").getBytes());
+			dout.flush();
 
-			sendMsg("REDY", dout);
-			
-			str = recMsg(dis);
-			
-			sendMsg("GETS All", dout);
-			
-			str = recMsg(dis);
+			str = dis.readLine();
+			System.out.println("Server: " + str);
 
+			System.out.println("Client: REDY\n");
+			dout.write(("REDY\n").getBytes());
+			dout.flush();
+
+			str = dis.readLine();
+			System.out.println("Server: " + str);
+
+			System.out.println("Client: GETS All\n");
+			dout.write(("GETS All\n").getBytes());
+			dout.flush();
+
+			str = dis.readLine(); 
 			String[] strSplit = str.split(" ");
-			
+
 			int numOfServers = Integer.parseInt(strSplit[1]);
-			
-			sendMsg("OK", dout);
-			
+
+			System.out.println("Client: OK\n");
+			dout.write(("OK\n").getBytes());
+			dout.flush();
+
 			int LSCores = 0;
 			int LSid = 0;
-			
-			System.out.println("Server:\n");
+
+			System.out.println("Server: \n");
 			for (int i = 0; i < numOfServers; i++) {
-				str = recMsg(dis);
+				str = dis.readLine(); 
+				System.out.println(str);
+
 				strSplit = str.split(" ");
-				
+
+		// Loop that finds the largest server (amount of cores & its id)
+
 				if (Integer.parseInt(strSplit[4]) > LSCores) {
 					LSCores = Integer.parseInt(strSplit[4]);
 					LSid = Integer.parseInt(strSplit[1]);
 				}
 			}
-			
+
 			System.out.println("\n The Largest Server is "+LSid+"(id) with "+LSCores+" Cores.\n");
-			
-			sendMsg("OK", dout);
-			
-			str = recMsg(dis);
-			
-			sendMsg("QUIT", dout);
-			
-			str = recMsg(dis);
-			
+
+			System.out.println("Client: OK\n");
+			dout.write(("OK\n").getBytes());
+			dout.flush();
+
+			str = dis.readLine();
+			System.out.println("Server: " + str);
+
+			System.out.println("Client: QUIT\n");
+			dout.write(("QUIT\n").getBytes());
+			dout.flush();
+
+			str = dis.readLine();
+			System.out.println("Server: " + str +"\n");
+
 			dout.close();  
 			dis.close();
 			s.close();  
 		} catch(Exception e) { System.out.println(e); }  
 	}  
-
-	/* 
-	 * Sends a message through the data output stream dout
-	 * Automatically:
-	 * 		Puts end of line character \n 
-	 * 		Flushes()
-	 * 		Prints to console
-	 */
-	static void sendMsg(String msg, DataOutputStream inDout) {
-
-		inDout.write((msg+"\n").getBytes());
-		inDout.flush();
-
-		System.out.println("Client says: "+msg);
-	}
-
-	/*
-	 * Receives a message through the BufferedReader dis.
-	 * Automatically:
-	 * 		Reads the line and prints to console
-	 * 		Returns the line read in a string.
-	 */
-	static String recMsg(BufferedReader inDis) {
-
-		String inStr = (String)inDis.readLine();
-		System.out.println("Server says: "+inStr);
-
-		return inStr;
-	}
-}  
+}
