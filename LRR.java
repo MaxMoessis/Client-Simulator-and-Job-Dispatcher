b42633464cd2e0457834c3359689a95bf2c35929
COMP3100 Maximus Moessis <maximus.moessis@students.mq.edu.au>
2023-03-30 06:09:47 +0000

    Added functions for receving and sending messages to the stream which automate tedious procedures

--- LRR.java
+++ LRR.java
@@ -1,4 +1,5 @@
-import java.io.*;  
+import java.io.*;
+import java.lang.annotation.Documented;
 import java.net.*; 
  
 public class LRR {  
@@ -10,49 +11,60 @@ public class LRR {
 			
 			String str; // String used to read the BufferedReader Stream.
 
-			System.out.println("Client: HELO\n");
-			dout.write(("HELO\n").getBytes());  
-			dout.flush();  
+			// System.out.println("Client: HELO\n");
+			// dout.write(("HELO\n").getBytes());  
+			// dout.flush(); 
+			dout.sendMsg("HELO"); 
 		
-			str = (String)dis.readLine();
-			System.out.println("Server: " + str);
+			// str = (String)dis.readLine();
+			// System.out.println("Server: " + str);
+			str = dis.recMsg();
 			
 			String username = System.getProperty("user.name"); 
 			
-			System.out.println("Client: AUTH pooch\n");
-			dout.write(("AUTH "+ username +"\n").getBytes());
-			dout.flush();
+			// System.out.println("Client: AUTH pooch\n");
+			// dout.write(("AUTH "+ username +"\n").getBytes());
+			// dout.flush();
+			dout.sendMsg("AUTH "+ username);
 			
-			str = dis.readLine();
-			System.out.println("Server: " + str);
+			// str = dis.readLine();
+			// System.out.println("Server: " + str);
+			str = dis.recMsg();
 			
-			System.out.println("Client: REDY\n");
-			dout.write(("REDY\n").getBytes());
-			dout.flush();
+			// System.out.println("Client: REDY\n");
+			// dout.write(("REDY\n").getBytes());
+			// dout.flush();
+			dout.sendMsg("REDY");
 			
-			str = dis.readLine();
-			System.out.println("Server: " + str);
+			// str = dis.readLine();
+			// System.out.println("Server: " + str);
+			str = dis.recMsg();
 			
-			System.out.println("Client: GETS All\n");
-			dout.write(("GETS All\n").getBytes());
-			dout.flush();
+			// System.out.println("Client: GETS All\n");
+			// dout.write(("GETS All\n").getBytes());
+			// dout.flush();
+			dout.sendMsg("GETS All");
 			
-			str = dis.readLine(); 
+			// str = dis.readLine(); 
+			str = dis.recMsg();
+
 			String[] strSplit = str.split(" ");
 			
 			int numOfServers = Integer.parseInt(strSplit[1]);
 			
-			System.out.println("Client: OK\n");
-			dout.write(("OK\n").getBytes());
-			dout.flush();
+			// System.out.println("Client: OK\n");
+			// dout.write(("OK\n").getBytes());
+			// dout.flush();
+			dout.sendMsg("OK");
 			
 			int LSCores = 0;
 			int LSid = 0;
 			
 			System.out.println("Server: \n");
 			for (int i = 0; i < numOfServers; i++) {
-				str = dis.readLine(); 
-				System.out.println(str);
+				// str = dis.readLine(); 
+				// System.out.println(str);
+				str = dis.recMsg();
 				
 				strSplit = str.split(" ");
 				
@@ -66,23 +78,54 @@ public class LRR {
 			
 			System.out.println("\n The Largest Server is "+LSid+"(id) with "+LSCores+" Cores.\n");
 			
-			System.out.println("Client: OK\n");
-			dout.write(("OK\n").getBytes());
-			dout.flush();
+			// System.out.println("Client: OK\n");
+			// dout.write(("OK\n").getBytes());
+			// dout.flush();
+			dout.sendMsg("OK");
 			
-			str = dis.readLine();
-			System.out.println("Server: " + str);
+			// str = dis.readLine();
+			// System.out.println("Server: " + str);
+			str = dis.recMsg();
 			
-			System.out.println("Client: QUIT\n");
-			dout.write(("QUIT\n").getBytes());
-			dout.flush();
+			// System.out.println("Client: QUIT\n");
+			// dout.write(("QUIT\n").getBytes());
+			// dout.flush();
+			dout.sendMsg("QUIT");
 			
-			str = dis.readLine();
-			System.out.println("Server: " + str +"\n");
+			// str = dis.readLine();
+			// System.out.println("Server: " + str +"\n");
+			str = dis.recMsg();
 			
 			dout.close();  
 			dis.close();
 			s.close();  
 		} catch(Exception e) { System.out.println(e); }  
 	}  
+
+	/* 
+	 * Sends a message through the data output stream dout
+	 * Automatically:
+	 * 		Puts end of line character \n 
+	 * 		Flushes()
+	 * 		Prints to console
+	 */
+	public void sendMsg(String msg) {
+		this.write((msg+"\n").getBytes());
+		this.flush();
+
+		System.out.println("Client says: "+msg);
+	}
+
+	/*
+	 * Receives a message through the BufferedReader dis.
+	 * Automatically:
+	 * 		Puts end of line character \n
+	 * 		Prints to console
+	 */
+	public String recMsg() {
+		String inStr = (String)this.readLine();
+		System.out.println("Server says: "+inStr);
+
+		return inStr;
+	}
 }  
