00257e2e7d36fb1b417b97ce9540d7d5912acebe
COMP3100 Maximus Moessis <maximus.moessis@students.mq.edu.au>
2023-04-02 07:08:52 +0000

    LRR now produces the exact same log file as the ds-client (except for the starting command of course).

--- LRR.java
+++ LRR.java
@@ -1,6 +1,16 @@
 import java.io.*;
 import java.net.*; 
- 
+
+/* 
+ * Student Name: Maximus Moessis
+ * Student ID: 47083581
+ * 
+ * This client contains print statements that give additional information such as 
+ * which server is the largest, how many cores it has, and the number of those servers.
+ * Young Lee has requested the print statements be commented out for ease of marking, however
+ * I have left in the exception printers.  
+ */
+
 public class LRR {  
 	public static void main(String[] args) {  
 		try {      
@@ -14,19 +24,27 @@ public class LRR {
 			 * every time would be an unnecessary memory allocation.
 			 */
 			String str; 
+		
+
+			/* Initial Setup */
 
 			sendMsg("HELO", dout); 
 			recMsg(dis);
-			
 			String username = System.getProperty("user.name"); 
 			sendMsg("AUTH "+ username, dout);
 			recMsg(dis);
-
 			sendMsg("REDY", dout);
-			recMsg(dis);
+			/* To exactly mimick DS-client, the client must keep a record of the first job
+			 * so that it doesn't have to re-request it after the GETS information is retrieved. 
+			 */
+			String firstJob = recMsg(dis); 
+			String[] firstSplit = firstJob.split(" ");
+
+
+			/* Obtaining the Number of Servers */
+
 			sendMsg("GETS All", dout);
 			str = recMsg(dis);
-
 			String[] strSplit = str.split(" ");
 			
 			int numOfServers = Integer.parseInt(strSplit[1]);
@@ -34,16 +52,18 @@ public class LRR {
 			sendMsg("OK", dout);
 			
 			int LSCores = 0;
-			String serverName = "";
-			
-			String[] serverNames = new String[numOfServers];
+			String serverName = ""; // Name of the largest server
+			String[] serverNames = new String[numOfServers]; // Array containing all the server names.
+
+
+			/* Finding the largest server type and initialising array of server names */
 
 	//		System.out.println("\n Server List: \n");
 			for (int i = 0; i < numOfServers; i++) {
 				str = recMsg(dis);
 				strSplit = str.split(" ");
 
-				serverNames[i] = strSplit[0]; // getting a list of the server names
+				serverNames[i] = strSplit[0]; // initialising an array of the server names
 				
 				if (Integer.parseInt(strSplit[4]) > LSCores) {
 					LSCores = Integer.parseInt(strSplit[4]);
@@ -51,8 +71,11 @@ public class LRR {
 				}
 			}
 
-			int numOfLargest = 0; // Find out how many of that server type are present
+			/* Finding how many of the largest server type there are */
 
+			int numOfLargest = 0; // amount of the largest server type.
+
+			/* Every time the largest server name is found in the list of servers, increment the amount */
 			for(int i = 0; i < numOfServers; i++) {
 				if (serverNames[i].equals(serverName)) numOfLargest++;
 			}
@@ -62,17 +85,17 @@ public class LRR {
 	//		System.out.println("\n The Largest Server is "+serverName+" with "+LSCores+" Cores.\n");
 	//		System.out.println(" There are also "+numOfLargest+" of this type.\n");
 
-			/* SCHD - schedule a job 
-			 *	SYNOPSIS:
-			 *		SCHD jobID serverType serverID		
-			 *
-			 * JOBN
-			 * 	SYNOPSIS:
-			 * 		JOBN submitTime jobID estRuntime core memory disk
-			 */
+			/* The start of the Largest Round Robin algorithm */
 
-			short rr = 0;
+			int rr = 0; // Used to create round robin fashion (explained more inside loop).
 			str = "";
+
+			/* Scheduling the saved 'firstJob' */
+			if (recMsg(dis).equals(".")) { // receive the dot message after "OK" from the server listing.
+				sendMsg("SCHD "+firstSplit[2]+" "+serverName+" "+rr%numOfLargest, dout);
+				rr++;
+			}
+
 			while (!str.equals("NONE")) {  // Stop scheduling when there are no more jobs to schedule. 
 				recMsg(dis);
 				
@@ -80,30 +103,34 @@ public class LRR {
 				str = recMsg(dis);
 				strSplit = str.split(" ");
 
-				while (strSplit[0].equals("JCPL")) { // first check if it's JCPL
+				while (strSplit[0].equals("JCPL")) { // check if it's JCPL
 					sendMsg("REDY", dout);
 					str = recMsg(dis);
 					strSplit = str.split(" ");
 				}
 				
+				/* RR fashion is achieved by modding the 'rr' variable by the number of largest servers.
+				 * rr is then incremented after every SCHD, creating this round robin fashion. 
+				 * Once the loop is completed, this 'rr' variable also contains the total amount of jobs scheduled.
+				 */
 				if (strSplit[0].equals("JOBN")) { // if received JOBN then schedule the job. 
 					sendMsg("SCHD "+strSplit[2]+" "+serverName+" "+rr%numOfLargest, dout);
 					rr++;
 				}
+
 			}
+		//	System.out.println("\n The total amount of jobs scheduled was "+rr);
 
 			// Then close the connection
-
-			if (closeCon(dis, dout, s)) {
-	//			System.out.println("Connection successfully closed");
-			} else {
-	//			System.out.println("Connection failed to close");
-			}
+			closeCon(dis, dout, s);
 
 		} catch(Exception e) { 
 			System.out.println(e); 
 		}
 	}  
+
+
+	/* HELPER FUNCTIONS */
 
 	/* 
 	 * Sends a message through the data output stream dout
@@ -139,17 +166,19 @@ public class LRR {
 	 * Returns true if successfully closed. 
 	 */
 
-	static boolean closeCon(BufferedReader inDis, DataOutputStream inDout, Socket inS) throws IOException {
+	static void closeCon(BufferedReader inDis, DataOutputStream inDout, Socket inS) throws IOException {
 		try {
 			sendMsg("QUIT", inDout);			
 			recMsg(inDis);
 			inDout.close();  
 			inDis.close();
 			inS.close(); 
+
+//			System.out.println("Connection Closed!");
 		} catch (Exception e) {
 			System.out.println(e);
-			return false;
+
+//			System.out.println("Connection Failed to Close!");;
 		}
-		return true;
 	}
 }  
